0e3e8c205b3fba801d12b764cf0611ff32c14edb
elriya <riyabhoite1311@gmail.com>
2026-07-15 23:31:44 +0530

    Time: 1 ms (83.11%), Space: 42.9 MB (6.12%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        // The GCD of the sum of first n odds (n^2) and first n evens (n * (n + 1)) is always n.
+        return n;
+    }
+}
